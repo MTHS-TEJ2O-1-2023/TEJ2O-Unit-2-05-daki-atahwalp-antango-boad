f34cec0db102ca7af821f63dc0e0e7d03027796f
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-09-25 13:03:49 -0400

    Microbit find temperature (kelvin)

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Daki A.B
+ * Created on: Sep 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let randomNumber: number
+let temperature = input.temperature()
+
+basic.clearScreen()
+basic.pause(1000)
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    temperature = input.temperature()
+    basic.showNumber(input.temperature())
+    basic.showNumber(input.temperature() + 273.15) 
+    basic.showString('Kelvin')
+})
