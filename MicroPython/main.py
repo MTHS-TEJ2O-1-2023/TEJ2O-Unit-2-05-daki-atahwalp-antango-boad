b565f23428836fc7ba9e0673f30b412883b03e85
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-09-27 00:24:58 +0000

    MicroBit finds temp in Kelvin.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,18 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Daki A.B
+Created on: Sep 2023
+This module is Microbit finds temperature in Kelvin
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+temperature = temperature + 273
+
+while True:
+    if button_a.is_pressed():
+        display.show(temperature)
+        display.show("K")
